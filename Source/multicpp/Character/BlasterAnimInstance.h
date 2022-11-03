150a70658ced98482a48a9c863087b00db6b810c
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2022-11-03 18:28:39 +0530

    Class 39 Animation Blueprint
    Class 39 Animation Blueprint
    
    In this class I learned how to create animation blueprints in unreal engine with C++.

---
+++ Source/multicpp/Character/BlasterAnimInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlasterCharacter.h"
+#include "Animation/AnimInstance.h"
+#include "BlasterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTICPP_API UBlasterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta= (AllowPrivateAccess = "true"))
+		ABlasterCharacter* BlasterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta= (AllowPrivateAccess = "true"))
+		float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta= (AllowPrivateAccess = "true"))
+		bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta= (AllowPrivateAccess = "true"))
+		bool bIsAccelerating;
+
+	
+	
+};
--- Source/multicpp/Character/BlasterCharacter.cpp
+++ Source/multicpp/Character/BlasterCharacter.cpp
@@ -4,6 +4,7 @@
 #include "BlasterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 // Sets default values
 ABlasterCharacter::ABlasterCharacter()
 {
@@ -18,6 +19,9 @@ ABlasterCharacter::ABlasterCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 
 }
 
